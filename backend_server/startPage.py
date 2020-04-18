c9785f24ce12976c3f21392e2034896c9048d265
DDoockDDack <csj3684@gmail.com>
2020-04-19 00:17:50 +0900

    [feat] 사용자 계정 등록 및 연동 구현
    DB와 비교하여
    
    1. 회원가입
    2. 아이디 중복검사
    3. 로그인
    4. 로그인 후 사용자 정보 (ex. 컵 현황)
    
    기능 서버쪽 처리 구현

---
+++ backend_server/startPage.py
@@ -0,0 +1,128 @@
+from main import app
+from backend_server.dataBase import dateBase
+
+from flask import Blueprint, request
+import json
+
+
+@app.route('/signUp', method = ['GET', 'POST'])
+def createAccount():
+    db = dateBase()
+
+    phoneNumber = request.form['phoneNumber']
+    password = request.form['password']
+    name = request.form['name']
+
+    # DB : 회원으로 저장
+    sql = "INSERT INTO User(phoneNumber, password, name) VALUES(%s,%s,%s)"
+
+    try:
+        db.cursor.execute(sql, (phoneNumber, password, name))
+        db.connector.commit()
+
+    except mysql.connector.Error as e:
+        jsonDict = None
+        print("Error %d: %s" % (e.args[0], e.args[1]))
+
+    else:
+        jsonDict = {'phoneNumber' : phoneNumber,
+                    'password' : password,
+                    'name' : name}
+    finally:
+        db.dbDisconnect()
+
+    return json.dumps(jsonDict).encode('utf-8')
+
+@app.route('/signIn', method = ['GET', 'POST'])
+def signIn():
+    db = dateBase()
+
+    phoneNumber = request.form['phoneNumber']
+    password = request.form['password']
+
+    # DB : 회원인지 확인
+    sql = "SELECT * FROM User WHERE phoneNumber = %s and password = %s"
+
+    try:
+        db.cursor.execute(sql, (phoneNumber, password))
+
+        if db.cursor.fetchone() == None:
+            isSuccess = False
+        else:
+            isSuccess = True
+
+    except mysql.connector.Error as e:
+        jsonDict = None
+        print("Error %d: %s" % (e.args[0], e.args[1]))
+
+    else:
+        jsonDict = {'success' : isSuccess}
+
+    finally:
+        db.dbDisconnect()
+
+    return json.dumps(jsonDict).encode('utf-8')
+
+@app.route('/duplicateCheck', method = ['GET', 'POST'])
+def duplicateCheck():
+    db = dateBase()
+
+    phoneNumber = request.form['phoneNumber']
+
+    # DB : 중복체크
+    sql = "SELECT * FROM User WHERE phoneNumber = %s"
+    try:
+        db.cursor.execute(sql, (phoneNumber))
+        if db.curs.fetchone() == None:
+            isDuplicated = False
+        else:
+            isDuplicated = True
+
+    except Exception as e:
+        jsonDict = None
+        print(e)
+
+    else:
+        jsonDict = {'duplicate': isDuplicated}
+
+    finally:
+        db.dbDisconnect()
+
+    return json.dumps(jsonDict).encode('utf-8')
+
+
+@app.route('/cupInfo/get', method = ['GET', 'POST'])
+def getCupInfo():
+    db = dateBase()
+
+    phoneNumber = request.form['phoneNumber']
+    jsonArray = []
+
+    sql = "SELECT * FROM User WHERE phoneNumber = %s"
+    try:
+        rows = db.cursor.execute(sql, (phoneNumber))
+    except Exception as e:
+        jsonArray = None
+        print(e)
+
+    else:
+        jsonArray.append({'cafeID': rows['cafeID'],
+                          'cafeName': rows['cafeName'],
+                          'cumNumber': rows['cupNumber'],
+                          'cafeLogo': rows['cafeLogo'],
+                          'dueDate': rows['dueDate']})
+
+    finally:
+        db.dbDisconnect()
+
+    return json.dumps(jsonArray).encode('utf-8')
+
+
+
+
+
+
+
+
+
+
