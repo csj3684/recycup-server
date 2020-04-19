6b33279876d319c75ad95456fde8179a5736cfd8
DDoockDDack <csj3684@gmail.com>
2020-04-19 13:17:07 +0900

    ;

--- testApplication.py
+++ testApplication.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 if __name__=='__main__':
  app = Flask(__name__)
- app.run(host='localhost', port=8888, debug=True)
+ app.run(host='0.0.0.0', port=8888, debug=True)
 
 
  @app.route('/')
