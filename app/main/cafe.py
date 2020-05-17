3e23d56b2bf3a165276cd6b680970b259aa06ed1
seungjin_choi <csj3684@gmail.com>
2020-05-17 05:12:45 +0000

    [feat][style] 각 카페 위치 정보 로드 가능
    중간 구조 수정으로 인해
    
    Head : 본사정보 (체인점 이름, 로고 정보, 플라스틱 컵 종류)
    Cafe : 각 체인점 정보 (위도, 경도 포함)
    
    관련 코드 수정

--- app/main/cafe.py
+++ app/main/cafe.py
@@ -1,35 +1,55 @@
 from app.main.dataBase import dateBase
 from common import *
+import os
 
 cafe = Blueprint('cafe', __name__)
 
-@cafe.route('/cafeInfo', methods = ['GET', 'POST'])
-def getCafeInfo():
-
-    print("cafeInfo")
+@cafe.route('/headInfo/get', methods = ['GET', 'POST'])
+def getHeadInfo():
+    print("headInfo")
 
     db = dateBase()
-
-    cafeInfos = []
-
+    headInfos = []
     try:
-        db.cursor.execute("SELECT * FROM RecyCup.Cafe")
+        db.cursor.execute("SELECT * FROM RecyCup.Head")
         rows = db.cursor.fetchall()
         db.connector.commit()
     except Exception as e:
-        cafeInfos = None
-        print("Error in 'getCafeInfo'", e)
+        headInfos = None
+        print("Error in 'getCafeInfo'", e); print("\n\n")
 
     else:
         for row in rows:
-            cafeInfos.append({'logoPath': row['logoPath'],
-                              'cafeName': row['name'],
+            print(row)
+            headInfos.append({'headName': row['headID'],
+                              'logoPath': row['logoPath'],
                               'type': row['type']})
+    finally:
+        db.dbDisconnect()
+
+    return json.dumps(headInfos).encode('utf-8')
+
+@cafe.route('/cafeInfo/get', methods = ['GET', 'POST'])
+def getCafeInfo():
+    print("cafeInfo")
+
+    db = dataBase()
+    cafeInfos = []
+    try:
+        db.cursor.execute("select * from RecyCup.Cafe")
+        rows = db.cursor.fetchall()
+        
+    except Exception as e:
+        print("Error in 'getCafeInfo'", e); print("\n\n")
+
+    else:
+        for row in rows:
+            cafeInfos.append({'cafeID' : row['cafeID'],
+                              'location' : (row['latitude'], row['longitude'])})
+
     finally:
         db.dbDisconnect()
 
     return json.dumps(cafeInfos).encode('utf-8')
 
-
-
-
+    
